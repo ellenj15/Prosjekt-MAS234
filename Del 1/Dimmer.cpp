a83a246142383856f948e276541ac681841fa095
ellenj15 <32056037+ellenj15@users.noreply.github.com>
2017-10-31 11:23:28 +0100

    Rename main.cpp to Dimmer.cpp

--- Del 1/Dimmer.cpp
+++ Del 1/Dimmer.cpp
@@ -18,20 +18,20 @@ float analogRead;
 
 int main(void)
 {
-	cli();								// Slå av interrupts.
+	cli();								// SlÃ¥ av interrupts.
 	
 	DDRD	=	0x01;					// Setter port D til output.
 
-	OCR1A	=	199;					// Hvor langt han teller før interrupt ( 16*10^6 Hz/ ( 8 (prescaler) * 10 000 Hz ) ) - 1
+	OCR1A	=	199;					// Hvor langt han teller fÃ¸r interrupt ( 16*10^6 Hz/ ( 8 (prescaler) * 10 000 Hz ) ) - 1
 
 	TCCR1B |=	(1 << WGM12);			// "Clear on Timer Compare match" - Setter timer 1 til 0 etter at den har interruptet.
 	TCCR1B |=	(1 << CS11);			// Setter prescaler til 8.
-	TIMSK1 |=	(1 << OCIE1A);			// Gjør at timeren interrupter ved compare match.
+	TIMSK1 |=	(1 << OCIE1A);			// GjÃ¸r at timeren interrupter ved compare match.
 	
 	ADCSRA |= (1 << ADEN) | (1 << ADIE);
 	ADMUX |= (1 << REFS0);
 	
-	sei();								// Slå på interrupts.
+	sei();								// SlÃ¥ pÃ¥ interrupts.
 	
 	while(1){}
 }

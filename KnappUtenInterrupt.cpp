c73393e02cb3ac792b7b6a2ecc20bc8cc6fdac07
ellenj15 <32056037+ellenj15@users.noreply.github.com>
2017-10-31 11:35:32 +0100

    Add files via upload

---
+++ KnappUtenInterrupt.cpp
@@ -0,0 +1,26 @@
+﻿#include <avr/io.h>
+
+void loop(void);
+
+int main(void)
+{
+	DDRD = 0x01;
+	
+	while(1)
+	{
+		loop();
+	}
+}
+
+void loop (void)
+{
+	if((PINB &= (1 << PINB1)) == 2)
+	{
+		PORTD &= ~(1 << PD0);
+	}
+	
+	else
+	{
+		PORTD |= (1 << PD0);
+	}
+}
